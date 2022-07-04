7957202e07a156fd2550818592cbc5a793a8d130
Jorge Luis Tanos <jorgeluis168@hotmail.com>
2022-07-04 15:30:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Para ci/cd agregar webhooks en github
+
+```
+Payload URL:  + /github-webhook/
+Content Type: application/json
+Just the push event.
+```
---
+++ index.test.js
@@ -0,0 +1,16 @@
+const supertest = require('supertest');
+const {app, server} = require("./index");
+const api = supertest(app);
+
+describe("Get route", () => {
+  test("page should return hello world", async () => {
+    const res = await api.get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual("Run app express in heroku with jenkins!!!");
+  });
+});
+
+afterAll(() => {
+    // console.log(app)
+  server.close();
+});
